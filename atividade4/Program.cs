01c9b7fe750e18ccdb940adfb5d3b58ebf4276aa
Desenvolvedor <Desenvolvedor@P2200042290>
2023-09-26 18:16:42 -0300

    Add project files.

---
+++ atividade4/Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+decimal valor, soma = 0.0m;
+
+Console.WriteLine("Digite um número:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+for (decimal i = 1; i <= numero; i++)
+{
+    valor = 1 / i;
+    soma += valor;
+}
+Console.WriteLine(soma);
+
+
